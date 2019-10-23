79ac4b452d695e3681e54dc522d080ea35f70d80
mutatrum <mutatrum@gmail.com>
2019-10-23 21:50:31 +0200

    Usability improvements for iOS and color blindness

--- hodl.js
+++ hodl.js
@@ -1,3 +1,15 @@
+const COLORMAPS = [
+  ['#543005', '#8c510a', '#bf812d', '#dfc27d', '#f6e8c3', '#f5f5f5', '#c7eae5', '#80cdc1', '#35978f', '#01665e', '#003c30'],
+  ['#8e0152', '#c51b7d', '#de77ae', '#f1b6da', '#fde0ef', '#f7f7f7', '#e6f5d0', '#b8e186', '#7fbc41', '#4d9221', '#276419'],
+  ['#40004b', '#762a83', '#9970ab', '#c2a5cf', '#e7d4e8', '#f7f7f7', '#d9f0d3', '#a6dba0', '#5aae61', '#1b7837', '#00441b'],
+  ['#7f3b08', '#b35806', '#e08214', '#fdb863', '#fee0b6', '#f7f7f7', '#d8daeb', '#b2abd2', '#8073ac', '#542788', '#2d004b'],
+  ['#67001f', '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#f7f7f7', '#d1e5f0', '#92c5de', '#4393c3', '#2166ac', '#053061'],
+  ['#67001f', '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#ffffff', '#e0e0e0', '#bababa', '#878787', '#4d4d4d', '#1a1a1a'],
+  ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee090', '#ffffbf', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4', '#313695'],
+  ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#d9ef8b', '#a6d96a', '#66bd63', '#1a9850', '#006837'],
+  ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2'],
+];
+
 var prices;
 var since;
 
@@ -18,8 +30,12 @@ function onLoad() {
 }
 
 function init() {
+  if (localStorage.getItem("palette") === null) {
+    localStorage.palette = 7;
+  }
   drawIndex();
   drawHodl();
+  createLabels();
 }
 
 function drawIndex() {
@@ -33,16 +49,13 @@ function drawIndex() {
 }
 
 function createIndexGradient(indexContext) {
-  // http://colorbrewer2.org/?type=diverging&scheme=RdYlGn&n=11
   var indexGradient = indexContext.createLinearGradient(0, 0, 0, 400);
-  var colors = ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#d9ef8b', '#a6d96a', '#66bd63', '#1a9850', '#006837'];
   var stop = 0;
-    for (var color of colors) {
-      indexGradient.addColorStop(stop, color);
-      stop += 0.1;
-    }
-    
-    return indexGradient;
+  for (var color of COLORMAPS[localStorage.palette]) {
+    indexGradient.addColorStop(stop, color);
+    stop += 0.1;
+  }
+  return indexGradient;
 }
 
 function drawHodl() {
@@ -57,50 +70,42 @@ function drawHodl() {
   hodlCanvas.height = size;
   
   var hodlContext = hodlCanvas.getContext('2d');
-  drawPixels(hodlContext, size);
-  drawDates(hodlContext, size);
+  drawPixels(hodlContext);
+  drawDots(hodlContext);
   
+  hodlCanvas.style.backgroundColor = COLORMAPS[localStorage.palette][5]; 
   hodlCanvas.style.display = 'block';
 
   var aboutDiv = document.getElementById('about');
   aboutDiv.style.display = 'block';
 }
 
-function drawPixels(hodlContext, size) {
+function drawPixels(hodlContext) {
+  var size = prices.length;
   var colors = getColorLookup();
 
-  var imageData = hodlContext.createImageData(size, size);
+  var imageData = hodlContext.createImageData(size - 1, size);
   var buffer = new ArrayBuffer(imageData.data.length);
   var pixelMap = new Uint32Array(buffer);
-  var y = size;
+  var y = size - 1;
   for (var selldate = 1; selldate <= size; selldate++) {
     for (var buydate = 0; buydate < selldate; buydate++) {
       var profit = getProfit(buydate, selldate);
       pixelMap[y + buydate] = colors[getColorIndex(profit)];
     }
-    y += size;
+    y += size - 1;
   }
   
   imageData.data.set(new Uint8ClampedArray(buffer));
   hodlContext.putImageData(imageData, 0, 0);
 }
 
-function drawDates(hodlContext, size) {
-  var yearsDiv = document.getElementById('years');
-
+function drawDots(hodlContext) {
+  var size = prices.length;
   for (var i = 0; i < size; i++) {
     var date = getIndexDate(i);
     if (date.getDate() == 1) {
       if (date.getMonth() == 0) {
-        
-        var div = document.createElement('div');
-        div.classList.add('year');
-        div.style.top = (i - 15) + 'px';
-        div.style.left = (i +  15) + 'px';
-        div.innerHTML = date.getFullYear();
-
-        yearsDiv.appendChild(div);
-        
         hodlContext.moveTo(i + 0.5, i + 0.5);
         hodlContext.lineTo(i + 3.5, i - 2.5);
         hodlContext.stroke();
@@ -109,6 +114,23 @@ function drawDates(hodlContext, size) {
         hodlContext.lineTo(i + 1.5, i - 0.5);
         hodlContext.stroke();
       }
+    }
+  }
+}
+
+function createLabels() {
+  var yearsDiv = document.getElementById('years');
+  for (var i = 0; i < prices.length; i++) {
+    var date = getIndexDate(i);
+    if (date.getDate() == 1 && date.getMonth() == 0) {
+        
+      var div = document.createElement('div');
+      div.classList.add('year');
+      div.style.top = (i - 15) + 'px';
+      div.style.left = (i +  15) + 'px';
+      div.innerHTML = date.getFullYear();
+
+      yearsDiv.appendChild(div);
     }
   }
 }
@@ -240,5 +262,12 @@ function plural(n, word) {
 function onHelpClick(event) {
   var helpDiv = document.getElementById('help');
   helpDiv.style.display = (helpDiv.style.display == 'block' ? 'none' : 'block');
+  event.preventDefault();
+}
+
+function onChangePaletteClick(event) {
+  localStorage.palette = (localStorage.palette + 1) % COLORMAPS.length;
+  drawIndex();
+  drawHodl();
   event.preventDefault();
 }
