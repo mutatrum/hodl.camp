a10c61c3b2ebe25fcbaabeb6bfaec00068e22511
mutatrum <mutatrum@gmail.com>
2019-11-14 23:04:58 +0100

    Fix Firefox canvas fingerprinting security warning (@6102bitcoin)
    Migrated from reading css gradient pixel data to chroma.js for creating
    the color maps

--- hodl.js
+++ hodl.js
@@ -1,15 +1,5 @@
-const COLORMAPS = [
-  ['#543005', '#8c510a', '#bf812d', '#dfc27d', '#f6e8c3', '#f5f5f5', '#c7eae5', '#80cdc1', '#35978f', '#01665e', '#003c30'],
-  ['#8e0152', '#c51b7d', '#de77ae', '#f1b6da', '#fde0ef', '#f7f7f7', '#e6f5d0', '#b8e186', '#7fbc41', '#4d9221', '#276419'],
-  ['#40004b', '#762a83', '#9970ab', '#c2a5cf', '#e7d4e8', '#f7f7f7', '#d9f0d3', '#a6dba0', '#5aae61', '#1b7837', '#00441b'],
-  ['#7f3b08', '#b35806', '#e08214', '#fdb863', '#fee0b6', '#f7f7f7', '#d8daeb', '#b2abd2', '#8073ac', '#542788', '#2d004b'],
-  ['#67001f', '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#f7f7f7', '#d1e5f0', '#92c5de', '#4393c3', '#2166ac', '#053061'],
-  ['#67001f', '#b2182b', '#d6604d', '#f4a582', '#fddbc7', '#ffffff', '#e0e0e0', '#bababa', '#878787', '#4d4d4d', '#1a1a1a'],
-  ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee090', '#ffffbf', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4', '#313695'],
-  ['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#d9ef8b', '#a6d96a', '#66bd63', '#1a9850', '#006837'],
-  ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2'],
-];
-const STOPS = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.625, 0.725, 0.85, 1.0];
+const COLORMAPS = ['BrBG', 'PiYG', 'PRGn', 'PuOr', 'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral'];
+const DOMAIN = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.625, 0.725, 0.85, 1.0];
 
 var prices;
 var since;
@@ -50,32 +40,29 @@ function init() {
 
   createLabels();
   setScale();
-  drawIndex();
-  drawHodl();
+
+  var colorMap = getColorMap();
+  drawIndex(colorMap);
+  drawHodl(colorMap);
 }
 
-function drawIndex() {
+function drawIndex(colorMap) {
   var colorMapCanvas = document.getElementById('colormap');
   var colorMapContext = colorMapCanvas.getContext('2d');
 
-  colorMapContext.fillStyle = createColorMap(colorMapContext);
-  colorMapContext.fillRect(0, 0, 25, 401);
+  var imageData = colorMapContext.createImageData(1, 401);
+  imageData.data.set(new Uint8ClampedArray(colorMap.buffer));
+  for (var i = 0; i < 25; i++) {
+    colorMapContext.putImageData(imageData, i, 0);
+  }
 
   colorMapCanvas.style.display = 'block';
 }
 
-function createColorMap(colorMapContext) {
-  var colorMapGradient = colorMapContext.createLinearGradient(0, 0, 0, 400);
-  for (var i = 0; i <= 10; i++) {
-    colorMapGradient.addColorStop(STOPS[i], COLORMAPS[localStorage.palette][i]);
-  }
-  return colorMapGradient;
-}
-
-function drawHodl() {
+function drawHodl(colorMap) {
   var size = prices.length - 1;
 
-  var backgroundColor = COLORMAPS[localStorage.palette][5];
+  var backgroundColor = getColorScale().colors()[5];
 
   var hodlCanvas = document.getElementById('hodl');
   hodlCanvas.style.backgroundColor = backgroundColor; 
@@ -84,7 +71,7 @@ function drawHodl() {
   hodlCanvas.height = size;
   
   var hodlContext = hodlCanvas.getContext('2d');
-  drawPixels(hodlContext);
+  drawPixels(hodlContext, colorMap);
   
   var backgroundDiv = document.getElementById('background');
   backgroundDiv.style.backgroundColor = backgroundColor; 
@@ -93,9 +80,8 @@ function drawHodl() {
   aboutDiv.style.display = 'block';
 }
 
-function drawPixels(hodlContext) {
+function drawPixels(hodlContext, colorMap) {
   var size = prices.length - 1;
-  var colorMap = getColorMap();
 
   var imageData = hodlContext.createImageData(size, size);
   var buffer = new ArrayBuffer(imageData.data.length);
@@ -140,10 +126,14 @@ function createLabels() {
 }
 
 function getColorMap() {
-  var colorMapCanvas = document.getElementById('colormap');
-  var colorMapContext = colorMapCanvas.getContext('2d');
-  imageData = colorMapContext.getImageData(0, 0, 1, 400);
-  return new Uint32Array(imageData.data.buffer);
+  var colorScale = getColorScale();
+  var colors = colorScale.mode('lab').domain(DOMAIN).colors(401, 'rgb');
+  var buffer = colors.map(rgb => (255 << 24) | (rgb[2] << 16) | (rgb[1] << 8) | rgb[0]);
+  return new Uint32Array(buffer);
+}
+
+function getColorScale() {
+  return chroma.scale(COLORMAPS[localStorage.palette]);
 }
 
 function onMouseLeave(event) {
@@ -271,8 +261,9 @@ function onHelpClick(event) {
 
 function onChangePaletteClick(event) {
   localStorage.palette = (localStorage.palette + 1) % COLORMAPS.length;
-  drawIndex();
-  drawHodl();
+  var colorMap = getColorMap();
+  drawIndex(colorMap);
+  drawHodl(colorMap);
   event.preventDefault();
 }
 
