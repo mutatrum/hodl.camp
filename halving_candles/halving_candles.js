c8fcb4c7d83dfb9f6cd8786868d6d5afd60b794f
mutatrum <mutatrum@gmail.com>
2020-02-17 13:23:34 +0100

    Code cleanup

--- halving_candles/halving_candles.js
+++ halving_candles/halving_candles.js
@@ -16,8 +16,18 @@ var resizeTimer;
 async function init() {
   var response = await fetch('../data.json');
   data = await response.json();
-  var size = data.bitcoin.length - 1;
 
+  createCandles();
+  
+  webSocketConnect();
+  
+  window.onresize = function(event) {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(drawCandles, 250);
+  }
+}
+
+function createCandles() {
   candles = new Array();
 
   var candle = createCandle(data.bitcoin[0]);
@@ -25,7 +35,7 @@ async function init() {
   var firstCandleIndex = data.halvings.findIndex(date => date.localeCompare(data.since) == 1) - 1;
   candles[firstCandleIndex] = candle;
   
-  for (var i = 0; i <= size; i++) {
+  for (var i = 0; i < data.bitcoin.length; i++) {
     
     var price = data.bitcoin[i];
     candle.h = Math.max(candle.h, price);
@@ -40,57 +50,6 @@ async function init() {
       candles[index] = candle;
     }
   }
-  candle.c = data.bitcoin[size];
-  
-  webSocketConnect();
-  
-  window.onresize = function(event) {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function() {
-      update(candles[candles.length-1].c);
-    }, 250);
-  }
-}
-
-function webSocketConnect() {
-  document.getElementById('event').innerHTML = 'connect';
-  
-  const webSocket = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
-
-  webSocket.onopen = function (event) {
-    document.getElementById('event').innerHTML = 'open';
-    let msg = JSON.stringify({ 
-      event: 'subscribe', 
-      channel: 'ticker', 
-      symbol: 'tBTCUSD' 
-    });
-
-    webSocket.send(msg); 
-  };
-  
-  webSocket.onclose = function(event) {
-    document.getElementById('event').innerHTML = `close ${event.code} ${event.reason}`;
-    setTimeout(webSocketConnect, 1000);
-  }
-  
-  webSocket.onerror = function(event) {
-    document.getElementById('event').innerHTML = `close ${event.code} ${event.reason}`;
-  }
-  
-  webSocket.onmessage = function(event) {
-    var data = JSON.parse(event.data);
-    if (Array.isArray(data)) {
-      var message = data[1];
-      if (Array.isArray(message)) {
-        update(message[6]);
-      }
-      document.getElementById('spinner').innerHTML = ' ' + spinner[spin];
-      spin = (spin + 1) % spinner.length;
-    }
-    else {
-      document.getElementById('event').innerHTML = data.event;
-    }
-  };
 }
 
 function createCandle(price) {
@@ -100,6 +59,62 @@ function createCandle(price) {
     l: price,
     c: price
   };
+}
+
+function webSocketConnect() {
+  setStatus('connect');
+  
+  const webSocket = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
+
+  webSocket.onopen = function (event) {
+    setStatus('open');
+    let msg = JSON.stringify({ 
+      event: 'subscribe', 
+      channel: 'ticker', 
+      symbol: 'tBTCUSD' 
+    });
+
+    webSocket.send(msg);
+  };
+  
+  webSocket.onclose = function(event) {
+    setStatus(`close ${event.code} ${event.reason}`);
+
+    setTimeout(webSocketConnect, 1000);
+  }
+  
+  webSocket.onerror = function(event) {
+    setStatus(`close ${event.code} ${event.reason}`);
+  }
+  
+  webSocket.onmessage = function(event) {
+    var data = JSON.parse(event.data);
+    if (Array.isArray(data)) {
+      var message = data[1];
+      if (Array.isArray(message)) {
+        
+        var price = message[6];
+        
+        var candle = candles[candles.length-1];
+        candle.h = Math.max(candle.h, price);
+        candle.l = Math.min(candle.l, price);
+        candle.c = price;
+        
+        drawCandles();
+      }
+      document.getElementById('spinner').innerHTML = ' ' + spinner[spin];
+      spin = (spin + 1) % spinner.length;
+    }
+    else {
+      setStatus(data.event);
+    }
+  };
+
+  document.getElementById('title').innerHTML = 'halving candles';
+}
+
+function setStatus(status) {
+  document.getElementById('status').innerHTML = status;
 }
 
 function getIndexDate(index) {
@@ -115,19 +130,7 @@ function formatDate(date) {
 var width;
 var height;
 
-function update(price) {
-  var candle = candles[candles.length-1];
-  candle.h = Math.max(candle.h, price);
-  candle.l = Math.min(candle.l, price);
-  candle.c = price;
-
-  var maxPrice = candles.reduce((max, candle) => Math.max(max, candle ? candle.h : 0), 0);
-  var minPrice = candles.reduce((min, candle) => Math.min(min, candle ? candle.l : 0), maxPrice);
-  var maxRange = Math.log10(maxPrice);
-  var minRange = Math.log10(minPrice);
-  
-  document.getElementById('title').innerHTML = 'halving candles';
-  
+function drawCandles() {
   var canvas = document.getElementById('halving_candles');
   
   width = canvas.scrollWidth;
@@ -135,130 +138,131 @@ function update(price) {
 
   canvas.width = width;
   canvas.height = width;
-  
 
+  const ctx = canvas.getContext('2d');
+  var small = width < 600;
+  ctx.font = (small ? "12" : "15") + "px Droid Sans Mono"; 
+  ctx.lineCap = 'round';
+  ctx.lineJoin = 'round';
+
+  clearCanvas(ctx);
+
+  var maxPrice = candles.reduce((max, candle) => Math.max(max, candle ? candle.h : 0), 0);
+  var minPrice = candles.reduce((min, candle) => Math.min(min, candle ? candle.l : 0), maxPrice);
+  var maxRange = Math.log10(maxPrice);
+  var minRange = Math.log10(minPrice);
+
+  var getX = i => (75 + (i * (width - 150) / candles.length));
   var scale = (height - 35) / (maxRange - minRange);
   var getY = price => (height - 25) - (scale * (Math.log10(price) - minRange));
 
-  const ctx = canvas.getContext('2d');
-  
-  if (width < 600) {
-    ctx.font = "12px Droid Sans Mono";
-  } else {
-    ctx.font = "15px Droid Sans Mono";
+  for (var i = Math.ceil(minRange); i < maxRange; i++) {
+    var price = Math.pow(10, i);
+    drawPriceIndex(ctx, getY, price);
+    drawHorizontalGrid(ctx, getY, price);
   }
-  
+
+  data.halvings.forEach((halving, i) => {
+    var x = getX(i);
+    if (i % 4 == 0) {
+      drawVerticalGrid(ctx, x);
+      if (!small) {
+        drawHalvingLabel(ctx, x, data.halvings[i]);
+      }
+    }
+    if (i % 4 == 2) {
+      var era = (i + 2) / 4;
+      drawHalvingLabel(ctx, x, `era ${era}`);
+    }
+  });
+
+  candles.forEach((candle, i) => {
+    drawCandle(ctx, getX, getY, i);
+  });
+
+  drawPriceLabel(ctx, getY);
+}
+
+function clearCanvas(ctx) {
   ctx.fillStyle = 'darkgrey';
   ctx.rect(0, 0, width, height);
   ctx.fill();
   ctx.stroke();  
-
-  ctx.lineCap = 'round';
-  ctx.lineJoin = 'round';
-  
-  for (var index = Math.ceil(minRange); index < maxRange; index++) {
-    drawPriceIndex(ctx, getY, Math.pow(10, index));
-  }
-
-  if (width > 600) {
-    drawHalvingLabel(ctx, 75, data.halvings[0]);
-  }
-  drawHalvingLine(ctx, 75, height);
-
-  var w = (width - 150) / candles.length;
-  candles.forEach((candle, i) => {
-    var x = 75 + ((i + 0.5) * w);
-    
-    if (i % 4 == 1) {
-      var era = ((i - 1) / 4) + 1;
-      drawHalvingLabel(ctx, x + (w / 2), `era ${era}`);
-    }
-
-    if (i % 4 == 3) {
-      drawHalvingLine(ctx, x + (w / 2));
-      if (width > 600) {
-        drawHalvingLabel(ctx, x + (w / 2), data.halvings[i+1]);
-      }
-    }
-    
-    drawCandle(ctx, getY, x, w, candles[i]);
-  });
-
-  if (price) {
-    ctx.lineWidth = 1;
-    var y = getY(price);
-    ctx.textAlign = 'left';
-    ctx.fillStyle = 'black';
-    ctx.fillText(Math.round(price), width - 60, y + 5);
-
-    ctx.beginPath();
-    ctx.strokeStyle = 'black';
-    ctx.moveTo(width - 70, y);
-    ctx.lineTo(width - 65, y);
-    ctx.stroke();
-  }
+  ctx.fillStyle = 'black';
 }
 
 function drawPriceIndex(ctx, getY, price) {
-  ctx.lineWidth = 1;
   var y = getY(price);
   ctx.textAlign = 'right';
-  
-  ctx.fillStyle = 'black';
-  
   ctx.fillText(price, 60, y + 5);
   ctx.beginPath();
-  ctx.strokeStyle = 'black';
   ctx.moveTo(65, y);
   ctx.lineTo(70, y);
   ctx.stroke();
+}
 
-  ctx.beginPath();
+function drawHorizontalGrid(ctx, getY, price) {
+  var y = getY(price);
   ctx.setLineDash([1, 5]);
+  ctx.beginPath();
   ctx.moveTo(75, y);
   ctx.lineTo(width-75, y);
   ctx.stroke();
-
   ctx.setLineDash([]);
 }
 
-function drawCandle(ctx, getY, x, w, candle) {
+function drawCandle(ctx, getX, getY, i) {
   ctx.lineWidth = 2;
-  ctx.setLineDash([]);
-  
-  var yo = getY(candle.o);
-  var yh = getY(candle.h);
-  var yl = getY(candle.l);
-  var yc = getY(candle.c);
-  
-  var up = candle.c >= candle.o;
+
+  var candle = candles[i];
+
+  var up = candle.c > candle.o;
   ctx.strokeStyle = up ? 'green' : 'red';
   ctx.fillStyle = up ? 'darkgrey' : 'red';
+  
+  var x = getX(i + 0.5);
+  var w = getX(i + 1) - x;
+  var y = getY(candle.o);
+  var h = getY(candle.c) - y;
+  
   ctx.beginPath();
-  ctx.moveTo(x, yh);
-  ctx.lineTo(x, up ? yc : yo);
-  ctx.moveTo(x, up ? yo : yc);
-  ctx.lineTo(x, yl);
+  ctx.moveTo(x, getY(candle.h));
+  ctx.lineTo(x, getY(candle.l));
   ctx.stroke();
   
   ctx.beginPath();
-  ctx.rect(x - (w / 8), yo, w / 4, yc - yo);
+  ctx.rect(x - (w / 4), y, w / 2, h);
   ctx.fill();
   ctx.stroke();
-}
 
-function drawHalvingLine(ctx, x) {
   ctx.lineWidth = 1;
   ctx.strokeStyle = 'black';
-  ctx.beginPath();
+  ctx.fillStyle = 'black';
+}
+
+function drawVerticalGrid(ctx, x) {
   ctx.setLineDash([1, 5]);
+  ctx.beginPath();
   ctx.moveTo(x, 10);
   ctx.lineTo(x, height - 25);
   ctx.stroke();
+  ctx.setLineDash([]);
 }
 
 function drawHalvingLabel(ctx, x, label) {
-  ctx.fillStyle = 'black';
   ctx.textAlign = 'center';
   ctx.fillText(label, x, height - 10);
+}
+
+function drawPriceLabel(ctx, getY) {
+  var price = candles[candles.length-1].c;
+  
+  var y = getY(price);
+  ctx.textAlign = 'left';
+  ctx.fillText(Math.round(price), width - 60, y + 5);
+
+  ctx.beginPath();
+  ctx.moveTo(width - 70, y);
+  ctx.lineTo(width - 65, y);
+  ctx.stroke();
 }
