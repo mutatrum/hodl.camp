246c5fd76a5d61f70e2bf04fc81d4de59462ab85
mutatrum <mutatrum@gmail.com>
2020-09-21 13:33:30 +0200

    Added longest_streak

---
+++ longest_streak/longest_streak.js
@@ -0,0 +1,87 @@
+async function onLoad() {
+  init();
+}
+
+const WIDTH = 1200;
+const HEIGHT = 675;
+
+var data;
+
+async function init() {
+  var response = await fetch('../data.json');
+  data = await response.json();
+
+  createContent();
+}
+
+function createContent() {
+  const size = data.bitcoin.length - 1;
+  const content = document.getElementById('content');
+
+  var hasStreak;
+  var price = 0;
+
+  do {
+    hasStreak = false;
+    price += 1000;
+
+    var streakLength = 0;
+    var streakStart = 0;
+    var start = 0;
+
+    for (var index = 0; index <= size; index++) {
+      if (data.bitcoin[index] >= price) {
+        hasStreak = true;
+        if (start == 0) {
+          start = index;
+        }
+      } else {
+        if (start != 0) {
+          var length = index - start;
+          if (length > streakLength) {
+            streakStart = start;
+            streakLength = length;
+          }
+          start = 0;
+        }
+      }
+    }
+    if (start != 0 && index - start > streakLength) {
+      streakStart = start;
+      streakLength = size - start + 1;
+    }
+
+    content.innerHTML += '$' + price + ': ' + formatDays(streakLength);
+    if (hasStreak) {
+      content.innerHTML += ' (' + formatDate(getIndexDate(streakStart)) + ' - ';
+      if (streakStart + streakLength - 1 == size) {
+        content.innerHTML += 'today';
+      }
+      else {
+        content.innerHTML += formatDate(getIndexDate(streakStart + streakLength - 1));
+      }
+      content.innerHTML += ')';
+
+      if (start != 0 && start != streakStart) {
+        content.innerHTML += '&nbsp;&nbsp;Current streak: ' + formatDays(size - start + 1) + ' (' + formatDate(getIndexDate(start)) + ' - today)';
+      }
+
+      content.innerHTML += '</br>';
+    }
+  }
+  while (hasStreak);
+}
+
+function formatDays(longest) {
+  return longest + ' day' + (longest == 1 ? '' : 's');
+}
+
+function formatDate(date) {
+  return date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
+}
+
+function getIndexDate(index) {
+  var date = new Date(data.since);
+  date.setDate(date.getDate() + index);
+  return date;
+}
