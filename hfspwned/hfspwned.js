a478f180fbd0bd95dc2400cbc1d3f0a38e7f83ea
mutatrum <mutatrum@gmail.com>
2021-09-16 11:31:28 +0200

    Initial release of HFSPWNED

---
+++ hfspwned/hfspwned.js
@@ -0,0 +1,241 @@
+async function onLoad() {
+
+  websocketConnect()
+
+  var difficultyResponse = await fetch('difficulty.json')
+  difficultyData = await difficultyResponse.json()
+
+  var currentDifficultySpan = document.getElementById('current-difficulty')
+  currentDifficultySpan.setAttribute('data-difficulty', difficultyData.current)
+  currentDifficultySpan.setAttribute('data-difficulty-formatted', PARAMETERS.difficulty.format(difficultyData.current))
+
+  var maxDifficultySpan = document.getElementById('max-difficulty')
+  maxDifficultySpan.setAttribute('data-difficulty', difficultyData.max)
+  maxDifficultySpan.setAttribute('data-difficulty-formatted', PARAMETERS.difficulty.format(difficultyData.max))
+
+  var minerResponse = await fetch('miner.json')
+  minerData = await minerResponse.json()
+
+  const minerSelect = document.getElementById('miner')
+  for (var miner of minerData) {
+    var option = document.createElement('option')
+    option.text = miner.name
+    option.setAttribute('data-hashrate', miner.hashrate)
+    option.setAttribute('data-watt', miner.watt)
+    minerSelect.add(option)
+  }
+
+  var searchParams = new URLSearchParams(window.location.search)
+  if (searchParams.has('miner')) {
+    const minerSelect = document.getElementById('miner')
+    minerSelect.selectedIndex = [...minerSelect.options].findIndex (option => option.text === searchParams.get('miner'))
+  }
+  if (searchParams.has('x-axis')) document.getElementById('x-axis').value = searchParams.get('x-axis')
+  if (searchParams.has('y-axis')) document.getElementById('y-axis').value = searchParams.get('y-axis')
+  for(var parameter of Object.values(PARAMETERS)) {
+    var id = parameter.id
+    if (searchParams.has(id)) document.getElementById(id).value = searchParams.get(id)
+  }
+  if (searchParams.has('number')) document.getElementById('number').value = searchParams.get('number')
+  if (searchParams.get('price-sync')) {
+    document.getElementById('price-sync').checked = true
+    document.getElementById('price').disabled = true
+  }
+
+  onMinerUpdate()
+
+  new bootstrap.Popover(document.querySelector('.popover-dismiss'))
+
+  document.querySelectorAll('input[type=number]')
+    .forEach(input => input.addEventListener('wheel', () => {}))
+}
+
+function onChange() {
+  const minerSelect = document.getElementById('miner')
+  if (minerSelect.selectedIndex != 0) {
+    const minerOption = minerSelect.options[minerSelect.selectedIndex]
+    if (minerOption &&
+      ((minerOption.getAttribute('data-hashrate') != document.getElementById('hashrate').value) ||
+       (minerOption.getAttribute('data-watt') != document.getElementById('watt').value))) {
+          minerSelect.selectedIndex = 0
+    }
+  }
+
+  const rows = document.getElementById('table').rows
+
+  const selectXAxis = document.getElementById('x-axis')
+  const parameterXAxis = PARAMETERS[selectXAxis.value]
+  for (var i = 1, th; th = rows[0].cells[i]; i++) {
+    th.innerHTML = parameterXAxis.format(parameterXAxis.step(i))
+  }
+
+  const selectYAxis = document.getElementById('y-axis')
+  const parameterYAxis = PARAMETERS[selectYAxis.value]
+  for (var i = 1, tr; tr = rows[i]; i++) {
+    tr.cells[0].innerHTML = parameterYAxis.format(parameterYAxis.step(i))
+  }
+
+  for (var i = 0, option; option = selectXAxis.options[i]; i++) {
+    option.disabled = i == selectYAxis.selectedIndex;
+  }
+  for (var i = 0, option; option = selectYAxis.options[i]; i++) {
+    option.disabled = i == selectXAxis.selectedIndex;
+  }
+
+  for (var y = 1, row; row = rows[y]; y++) {
+    for (var x = 1, cell; cell = row.cells[x]; x++) {
+
+      var h = getValue(PARAMETERS.hashrate, x, y)
+      var f = 1 - (getValue(PARAMETERS.fee, x, y) / 100)
+      var s = getValue(PARAMETERS.subsidy, x, y)
+      var p = getValue(PARAMETERS.price, x, y)
+      var w = getValue(PARAMETERS.watt, x, y)
+      var n = document.getElementById('number').value
+      var e = getValue(PARAMETERS.energy, x, y)
+      var d = getValue(PARAMETERS.difficulty, x, y)
+
+      var result = n * (((h * f * s * p * 4383) / (d * 131072 / 18310546875)) - w * e * 1461 / 2000)
+
+      cell.setAttribute('data-amount', PARAMETERS.price.format(result))
+    }
+  }
+}
+
+function getValue(parameter, x, y) {
+  const id = parameter.id
+  if (document.getElementById('x-axis').value == id) return parameter.step(x)
+  if (document.getElementById('y-axis').value == id) return parameter.step(y)
+  return document.getElementById(id).value
+}
+
+function onMinerUpdate() {
+  const minerSelect = document.getElementById('miner')
+  const minerOption = minerSelect.options[minerSelect.selectedIndex]
+  if (minerOption) {
+    for(var parameter of Object.values(PARAMETERS)) {
+      var attribute = minerOption.getAttribute('data-' + parameter.id)
+      if (attribute) {
+        document.getElementById(parameter.id).value = attribute
+      }
+    }
+  }
+  onChange()
+}
+
+function onShareClick(event) {
+  var href = window.location.href
+  var url = new URL(href.slice(0, href.indexOf('?')))
+  var searchParams = url.searchParams
+  searchParams.append('x-axis', document.getElementById('x-axis').value)
+  searchParams.append('y-axis', document.getElementById('y-axis').value)
+  var minerSelectedIndex = document.getElementById('miner').selectedIndex
+  if (minerSelectedIndex > 0) {
+    searchParams.append('miner', document.getElementById('miner').options[minerSelectedIndex].value)
+  }
+  for(var parameter of Object.values(PARAMETERS)) {
+    searchParams.append(parameter.id, document.getElementById(parameter.id).value)
+  }
+  searchParams.append('number', document.getElementById('number').value)
+  if (document.getElementById('price-sync').checked) {
+    searchParams.append('price-sync', 'true')
+  }
+
+  navigator.clipboard.writeText(url.toString())
+
+  event.preventDefault()
+}
+
+function websocketConnect() {
+
+  const currentPriceSpan = document.getElementById('current-price')
+  const priceInput = document.getElementById('price')
+  const priceSyncCheckbox = document.getElementById('price-sync')
+
+  const webSocket = new WebSocket('wss://api-pub.bitfinex.com/ws/2')
+
+  webSocket.onopen = function (event) {
+    let msg = JSON.stringify({
+      event: 'subscribe',
+      channel: 'ticker',
+      symbol: 'tBTCUSD'
+    })
+
+    webSocket.send(msg)
+  }
+
+  webSocket.onclose = function(event) {
+    setTimeout(webSocketConnect, 1000)
+  }
+
+  webSocket.onmessage = function(event) {
+    var data = JSON.parse(event.data)
+    if (Array.isArray(data)) {
+      var message = data[1]
+      if (Array.isArray(message)) {
+        var price = message[6]
+        currentPriceSpan.setAttribute('data-price', price)
+        currentPriceSpan.setAttribute('data-price-formatted', PARAMETERS.price.format(price))
+        if (priceSyncCheckbox.checked) {
+          priceInput.value = price
+          onChange()
+        }
+      }
+    }
+  }
+}
+
+function onPriceClick(event) {
+  const element = event.target
+  document.getElementById('price').value = element.getAttribute('data-price')
+  onChange()
+}
+
+function onDifficultyClick(event) {
+  const element = event.target
+  document.getElementById('difficulty').value = element.getAttribute('data-difficulty')
+  onChange()
+}
+
+function onPriceSyncClick(event) {
+  const element = event.target
+  const priceInput = document.getElementById('price')
+  if (element.checked) {
+    priceInput.value = document.getElementById('current-price').getAttribute('data-price')
+    priceInput.disabled = true
+  } else {
+    priceInput.disabled = false
+  }
+}
+
+class Parameter {
+  constructor(id, format, step) {
+    this.id = id
+    this.format = format
+    if (step) this.step = step
+  }
+
+  step(i) {
+    var value = document.getElementById(this.id).value
+    var step = Number(Number(value / 5).toPrecision(1))
+    return i * step
+  }
+}
+
+const PARAMETERS = {
+  hashrate: new Parameter('hashrate', v => v.toLocaleString() + ' Th/s'),
+  fee: new Parameter('fee', v => v.toLocaleString() + '%'),
+  subsidy: new Parameter('subsidy', v => v, subsidyStep),
+  price: new Parameter('price', v => formatPrice(Math.round(v))),
+  watt: new Parameter('watt', v => v.toLocaleString() + ' W'),
+  energy: new Parameter('energy', formatPrice),
+  difficulty: new Parameter('difficulty', v => v.toLocaleString())
+}
+
+function formatPrice(v) {
+  return (v < 0 ? '-' : '') + '$' + Math.abs(v).toLocaleString()
+}
+
+function subsidyStep(i) {
+  var s = (100 / (1 << i)).toString()
+  return s.slice(0, (s.indexOf('.')) + 9)
+}
